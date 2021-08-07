3429ff041aef212a2d9281e99e5d671704233b34
Sirius0v0 <1694605269@qq.com>
2021-08-08 00:35:23 +0800

    24点游戏

--- index.js
+++ index.js
@@ -17,7 +17,14 @@ bot.on("system.login.qrcode", function (e) {
 
 exports.bot = bot
 
-// plugins
+/* ====== Plugins ====== */
+
+// test plugins
 require("./src/app-plugin/others/plugin-hello") //hello world
 require("./src/app-plugin/others/plugin-image") //发送图文和表情
+
+// sys
 require("./src/sys/plugin-online") //监听上线事件
+
+// games
+require("./src/app-plugin/games/24points/plugin-24points") // 24点游戏
---
+++ src/app-plugin/games/24points/test.js
@@ -0,0 +1,3 @@
+let games = require("./TwentyFourPoints");
+let res = Math.floor(Math.random() * 4);
+console.log(res)
