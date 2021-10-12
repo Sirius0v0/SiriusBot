0b36250199d4b71435140196d8918142be08d07f
Sirius0v0 <1694605269@qq.com>
2021-10-13 01:21:33 +0800

    增加生成音乐内容

--- plugins/musicgen/plugin-musicgen.js
+++ plugins/musicgen/plugin-musicgen.js
@@ -17,6 +17,8 @@ const help = `
 <-musicgen [Options] 乐谱>: 生成音乐,可选参数如下：
   -b, --bpm <正整数>: 音乐速度
   -s, --save <音乐名字> : 保存音乐
+  -i, --instrument <音色编号>：音色
+  -m, --major <曲调>：音乐曲调
 【乐谱不可有空格，可用|分割】 
 
 [音长写法规则]
@@ -40,6 +42,7 @@ const help = `
 ※ 修饰符位于一个音符之后。_将音符长度减半，n个连续的-和*分别将长度增加n和(1 - 1 / 2^n)倍。也就是说这三个修饰符分别相当于简谱中的下划线、横线和附点.
 ※ 修饰符会影响{}中的所有音符。比如{123}//和1//2//3//是等价的、{1234}_和1_2_3_4_是等价的.
 ※ 表示连音使用修饰符/，n个/将音符长度除以(n + 1).
+※ \\v{num}表示第num声部，例：\\v{1}1234567\\v{2}7654321
 `.trim();
 
 async function musicgen(_bot, data, args = null) {
@@ -61,9 +64,8 @@ async function musicgen(_bot, data, args = null) {
             return;
         }
         let m = musicData.getMusic(args[1]);
-        data.reply(m.slice(1 + m.indexOf("|")));
+        data.reply(m.slice(3 + m.indexOf("|||")));
         return;
-
     } else if (args?.length === 2 && ["play", '播放'].indexOf(args?.[0]) !== -1) {
         let musicData = new Data();
         let nameList = musicData.getMusicName();
@@ -88,14 +90,16 @@ async function musicgen(_bot, data, args = null) {
         data.reply(opts.errMsg);
         return;
     }
-    let genRes = genMidi(`\\bpm{${opts.bpm}}|${opts.input}`, String(data.time));
+    let genRes = genMidi(`\\major{${opts.major}}\\bpm{${opts.bpm}}\\instrument{${opts.instrument}}|||${opts.input}`, String(data.time));
     if (!genRes.res) {
         data.reply(genRes.errMsg);
         return;
     }
     lastTime = data.time;// 成功调用则更新调用时间
     if (opts.dump) {   // 保存
-        new Data().updateMusic(opts.dump, `\\bpm{${opts.bpm}}|${opts.input}`);
+        let status = new Data().updateMusic(opts.dump, `\\major{${opts.major}}\\bpm{${opts.bpm}}\\instrument{${opts.instrument}}|||${opts.input}`);
+        if (status) data.reply("保存成功");
+        else data.reply("保存失败，名字已存在");
     }
 
     // let track = addNote(new MidiWriter.Track().setTempo(100), origin);
@@ -110,13 +114,20 @@ function genMidi(input, name) {
     let info = { res: false, errMsg: null };
     let ctx = qmidi.createContext();
     let lines = input.split(/\n|\r\n|\r/);
-    let midiFile = ctx.parse(input);
+    let midiFile;
+    try {
+        midiFile = ctx.parse(input);
+    } catch (error) {
+        info.errMsg = `Error: 乐谱错误\n错误标记：null`;
+        return info;
+    }
     let errors = ctx.getErrors();
     if (ctx.hasError()) {
         // for (let e of ctx.getPrintedErrors({ getLine: i => lines[i - 1] })) {
         //     console.log(e);
         // }
-        info.errMsg = `谱子有误，你是不是多写了【${errors[0]["range"]["text"]}】?`;
+        info.errMsg = `Error: ${(errors[0]["msg"].startsWith("BPM") ||
+            errors[0]["msg"].startsWith("Tempo")) ? "bpm参数非法或越界[4,16777215]" : "乐谱错误"}\n错误标记：${errors[0]["range"]["text"]}`;
         return info;
     }
     else {
@@ -129,7 +140,7 @@ function genMidi(input, name) {
 }
 
 function parseArgs(args) {
-    let opts = { input: null, bpm: 120, errMsg: null, dump: null };
+    let opts = { input: null, bpm: 120, instrument: 0, major: 'C', errMsg: null, dump: null };
     while (args.length) {
         if (args[0].charAt(0) === '-') {
             switch (args[0]) {
@@ -143,10 +154,31 @@ function parseArgs(args) {
                     }
                     opts.bpm = bpm;
                     break;
+                case '-i':
+                case '--instrument':
+                    args.shift();
+                    let instrument = Number(args.shift());
+                    if (!Number.isInteger(instrument)) {
+                        opts.errMsg = `参数错误：音色参数-i为整数,请查阅http://www.music.mcgill.ca/~ich/classes/mumt306/StandardMIDIfileformat.html#BMA1_4`;
+                        return opts;
+                    }
+                    opts.instrument = instrument;
+                    break;
                 case '-s':
                 case '--save':
                     args.shift();
                     opts.dump = args.shift();
+                    break;
+                case '-m':
+                case '--Major':
+                    args.shift();
+                    let major = args.shift();
+                    const majorList = ['C', 'D', 'E', 'F', 'G', 'A', 'B', 'C#', 'Cb', 'Db', 'Eb', 'F#', 'Gb', 'Ab', 'Bb'];
+                    if (majorList.indexOf(major) === -1) {
+                        opts.errMsg = `参数错误：曲调参数-m为表中其一[C, D, E, F, G, A, B, C#, Cb, Db, Eb, F#, Gb, Ab, Bb]`;
+                        return opts;
+                    }
+                    opts.major = major;
                     break;
                 default:
                     opts.errMsg = `未知参数 ${args[0]}`;
@@ -177,7 +209,7 @@ async function sendMusic(data) {
     ls.on('close', async (code) => {
         await data.reply([segment.record(path.join(__dirname, `./cache/${String(data.time)}.wav`))])
         let files = fs.readdirSync(path.join(__dirname, "./cache"));
-        if (files.length > 4) {
+        if (files.length > 40) {
             emptyDir(path.join(__dirname, "./cache"));
         }
     });
